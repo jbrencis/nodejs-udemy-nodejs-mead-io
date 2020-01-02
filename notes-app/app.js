80f6b446aef0fd257fbd8731cab0c6a7c221fd21
Jevgenijs Brencis <jevgenijs.brencis@gmail.com>
2020-01-02 16:48:25 +0200

    initial commit

---
+++ notes-app/app.js
@@ -0,0 +1,44 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+
+const getNotes = require('./notes.js');
+
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a note',
+  handler: function() {
+    console.log('Adding note...');
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log('Removing a note...');
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: function() {
+    console.log('Listing notes...');
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log('Reading a note a note...');
+  }
+});
+
+console.log(yargs.argv);
