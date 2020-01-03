da775936b42b60d694ac1b52324518b015f631bd
Jevgenijs Brencis <jevgenijs.brencis@gmail.com>
2020-01-03 19:07:59 +0200

    added findNote

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,30 +1,26 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = () => 'Your notes...';
-
 const addNote = (title, body) => {
   const notes = loadNotes();
 
-  const duplicateNotes = notes.filter(note => {
-    return note.title === title;
-  });
+  // #1 const duplicateNotes = notes.filter(note => note.title === title); //'filter' will go over all 100500 notes
+  const duplicateNotes = notes.find(note => note.title === title); // will find the first occurance of 'note' and exit
 
-  if (duplicateNotes.length === 0) {
+  // #1 if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({ title, body });
     saveNotes(notes);
     console.log(chalk.green.inverse('A new note added'));
   } else {
-    console.log(chald.red.inverse('Note title already taken'));
+    console.log(chalk.red.inverse('Note title already taken'));
   }
 };
 
 const removeNote = title => {
   const notes = loadNotes();
 
-  const notesToKeep = notes.filter(note => {
-    return note.title !== title;
-  });
+  const notesToKeep = notes.filter(note => note.title !== title);
 
   if (notes.length > notesToKeep.length) {
     console.log(chalk.green.inverse('Note removed!'));
@@ -39,6 +35,25 @@ const saveNotes = notes => {
   return fs.writeFileSync('notes.json', dataJSON);
 };
 
+const listNotes = () => {
+  console.log(chalk.blue.inverse('Your notes:'));
+
+  const notes = loadNotes();
+  return notes.forEach(note => console.log(chalk.cyan(note.title)));
+};
+
+const readNote = title => {
+  const notes = loadNotes();
+  const note = notes.find(note => note.title === title);
+
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse('Note not found'));
+  }
+};
+
 const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync('notes.json');
@@ -50,7 +65,8 @@ const loadNotes = () => {
 };
 
 module.exports = {
-  getNotes,
   addNote,
-  removeNote
+  removeNote,
+  listNotes,
+  readNote
 };
