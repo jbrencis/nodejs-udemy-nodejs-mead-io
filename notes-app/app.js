ada90603fb436ceb1f5edb4dc8d6de55d466aa81
Jevgenijs Brencis <jevgenijs.brencis@gmail.com>
2020-01-03 14:48:27 +0200

    added removeNote logic

--- notes-app/app.js
+++ notes-app/app.js
@@ -31,8 +31,15 @@ yargs.command({
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
-  handler: function() {
-    console.log('Removing a note...');
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: argv => {
+    notes.removeNote(argv.title);
   }
 });
 
